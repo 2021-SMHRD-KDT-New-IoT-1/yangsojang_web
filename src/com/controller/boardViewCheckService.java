fe803ed55602a925b902ffa7a7d3b5c2a62d8d7a
노소연 <92971204+soyeonie@users.noreply.github.com>
2021-12-22 12:32:15 +0900

    ㅁㅇㄹ
    Change-Id: Ief024c533b6a0d290f06272d976619095e6bfe6c

---
+++ src/com/controller/boardViewCheckService.java
@@ -0,0 +1,37 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/boardViewCheckService")
+public class boardViewCheckService extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+		request.setCharacterEncoding("euc-kr");
+		
+		HttpSession session = request.getSession();
+		
+		String mnt_seq = request.getParameter("mnt_seq");
+		int mnt_seq_int = Integer.parseInt(mnt_seq);
+				
+		if(mnt_seq_int > 0 ) {
+			System.out.println("관리 일지 view 페이지 이동 성공!");
+			
+			
+			// 세션 값 설정
+			session.setAttribute("mnt_seq_session", mnt_seq_int);
+			
+			response.sendRedirect("board_view.jsp");
+		}else {
+			System.out.println("관리 일지 view 페이지 이동 실패!");
+		}	
+	}
+
+}
